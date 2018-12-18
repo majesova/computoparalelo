e94f885c56654c0c764439010f7769b55b9a9dcc
Manuel <majesova@gmail.com>
2018-12-18 09:55:18 -0600

    mpi samples

--- MPI/HolaMPI2.c
+++ MPI/HolaMPI2.c
@@ -5,37 +5,24 @@
 
 #include <stdio.h>
 #include <mpi.h>
-#include <omp.h>
-
+#include <stdlib.h>
 int NCPU,tid,NPR,NTHR;
 
 int main(int argc, char* argv[])
 {
 
-#pragma omp parallel num_threads(2)
-  {
-    
-    NCPU = omp_get_num_procs();
-    tid = omp_get_thread_num();
-    NPR = omp_get_num_threads();
-    NTHR = omp_get_max_threads();
-	
 	int rank, size;
-	#pragma omp single
 	MPI_Init(&argc, &argv);
-	
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
-
 	char processor_name[MPI_MAX_PROCESSOR_NAME];
     int name_len;
-	
     MPI_Get_processor_name(processor_name, &name_len);
-	printf("Hello from thread %d out of %d from process %d out of %d on %s\n",tid, NPR, rank, NCPU, processor_name);
+	printf("Hello World from process %d of %d in procesor: %s\n", rank, size,processor_name);
 	MPI_Finalize();
+	
 
-  }
+
 	return 0;
 
 }
